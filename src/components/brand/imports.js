7721db8f8aa911f0257699c7ae463b9205e9143d
CR7sewey <miguelmotacarvalho@gmail.com>
2024-03-14 22:59:15 +0000

    Brand

---
+++ src/components/brand/imports.js
@@ -0,0 +1,7 @@
+import google from '../../assets/google.png';
+import slack from '../../assets/slack.png';
+import atlassian from '../../assets/atlassian.png';
+import dropbox from '../../assets/dropbox.png';
+import shoppify from '../../assets/shopify.png';
+
+export {google,slack,atlassian,dropbox,shoppify}
