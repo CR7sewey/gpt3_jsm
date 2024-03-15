8ca7e0d0f6d1be24e64f238a148121d17b2d089d
CR7sewey <miguelmotacarvalho@gmail.com>
2024-03-15 23:45:42 +0000

    CTA, possiblity, Blog + Article

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import './article.css';
+// p should be a Link to page
 
-const Article = () => {
+const Article = ({image, date, text}) => {
   return (
-    <div>
-      Article
+    <div className='gpt3__blog-container_article' id='article'>
+      <div className='gpt3__blog-container_article-image'>
+        <img src={image} alt="blog_image" />
+      </div>
+      <div className='gpt3__blog-container_article-content'>
+        <div>
+        <p>{date}</p>
+        <h3>{text}</h3>
+      </div>
+        <p>Read Full Article</p>
+      </div>
+
     </div>
   )
 }
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import {blog01,blog02,blog03,blog04,blog05} from './imports';
+import './blog.css';
+import { Article } from '../../components';
 
 const Blog = () => {
   return (
-    <div>
-      
+    <div className='gpt3__blog section__padding' id='blog'>
+      <div className='gpt3__blog-heading'>
+        <h1 className='gradient__text'>A lot is happening, <br /> We are blogging about it.</h1>
+      </div>
+      <div className='gpt3__blog-container'>
+        <div className='gpt3__blog-container_groupA'>
+        <Article image={blog01} date="Mar 15, 2024" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+      </div>
+      <div className="gpt3__blog-container_groupB">
+        <Article image={blog02} date="Mar 15, 2024" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+        <Article image={blog03} date="Mar 15, 2024" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+        <Article image={blog04} date="Mar 15, 2024" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+        <Article image={blog05} date="Mar 15, 2024" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+
+        </div>
+
+      </div>
     </div>
   )
 }
