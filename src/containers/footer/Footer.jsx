c0ea361e818bdfc79552a1f02ebc37ebef359f36
CR7sewey <miguelmotacarvalho@gmail.com>
2024-03-16 13:56:33 +0000

    Footer

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from 'react';
+import './footer.css';
+import gpt3Logo from '../../assets/logo.svg';
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className='gpt3__footer section__padding' id='footer'>
+      <div className='gpt3__footer-heading'>
+        <h1 className='gradient__text'>Do You want to step in to the<br />future before others?</h1>
+        
+      </div>
+      <div className='gpt3__footer-btn'>
+        <p>Request Early Access</p>
+      </div>
+      <div className='gpt3__footer-links'>
+        <div className='gpt3__footer-links_logo'>
+          <img src={gpt3Logo} />
+          <p>Portugal, 03/2024<br />Miguel Carvalho, All Rights Reserved</p>
+        </div>
+        <div className='gpt3__footer-links_div'>
+            <h4>Links</h4>
+            <p>Overons</p>
+            <p>Social Media</p>
+            <p>Counters</p>
+            <p>Contact</p>  
+        </div>
+        <div className='gpt3__footer-links_div'>
+            <h4>Company</h4>
+            <p>Terms & Conditions</p>
+            <p>Privacy Policy</p>
+            <p>Contact</p>
+        </div>
+        <div className='gpt3__footer-links_div'>
+            <h4>Get in touch</h4>
+            <p>Portugal</p>
+            <p>Coimbra</p>
+            <p>dummy@dummy.com</p>
+        </div>
+        
+      </div>
+      <div className='gpt3__footer-copyright'>
+        <p>© 2024 GPT-3. All rights reserved.</p>
+      </div>
+      
     </div>
   )
 }
